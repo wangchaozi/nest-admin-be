aa335a4411e41426c1d3baa83fdc041a1b3072d7
wangchaozi <2758213723@qq.com>
2024-05-08 14:49:46 +0800

    feat: "实体添加以项目初级模块划分"

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import envConfig from '../src/config/env';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleModule } from './modules/system/role/role.module';
+import { DeptModule } from './modules/system/dept/dept.module';
+import { MenuModule } from './modules/system/menu/menu.module';
+import envConfig from 'src/config/env';
 
 @Module({
   imports: [
-    AuthModule,
-    UserModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [envConfig.path],
@@ -19,17 +20,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        type: 'mysql', // 数据库类型
-        entities: [], // 数据表实体
+        type: 'mysql',
+        entities: [],  // 数据表实体
         host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
         port: configService.get<number>('DB_PORT', 3306), // 端口号
-        username: configService.get('DB_USER', 'root'), // 用户名
+        username: configService.get('DB_USER', 'root'),   // 用户名
         password: configService.get('DB_PASSWORD', '123456'), // 密码
-        database: configService.get('DB_DATABASE', 'nest-admin-be'), //数据库名
+        database: configService.get('DB_DATABASE', 'blog'), //数据库名
         timezone: '+08:00', //服务器上配置的时区
         synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
-      }),
+      })
     }),
+    RoleModule,
+    DeptModule,
+    MenuModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('nest-admin-be')
+    .setDescription('nest-admin-be-API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
